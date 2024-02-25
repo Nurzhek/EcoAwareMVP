8742c61b54d9ddf215710ca1ccba2d0641b3ccf2
Nurzhan <99636082+Nurzhek@users.noreply.github.com>
2024-02-25 23:14:29 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,26 @@ import 'package:google_maps_flutter/google_maps_flutter.dart'
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-//import 'package:myapp/page-1/onboard01.dart';
-// import 'package:myapp/page-1/onboard01-Tv6.dart';
-// import 'package:myapp/page-1/onboard01-7EW.dart';
-// import 'package:myapp/page-1/onboard01-3Yi.dart';
-// import 'package:myapp/page-1/main.dart';
-// import 'package:myapp/page-1/events.dart';
-// import 'package:myapp/page-1/new-events.dart';
-// import 'package:myapp/page-1/frame-2608829.dart';
-// import 'package:myapp/page-1/events-wG2.dart';
-// import 'package:myapp/page-1/forum.dart';
-// import 'package:myapp/page-1/event.dart';
-// import 'package:myapp/page-1/map.dart';
-// import 'package:myapp/page-1/newevent.dart';
-// import 'package:myapp/page-1/profile.dart';
-// import 'package:myapp/page-1/profile-Jog.dart';
+import 'package:myapp/page-1/onboard01.dart';
+import 'package:myapp/page-1/onboard01-Tv6.dart';
+import 'package:myapp/page-1/onboard01-7EW.dart';
+import 'package:myapp/page-1/onboard01-3Yi.dart';
+import 'package:myapp/page-1/main.dart';
+import 'package:myapp/page-1/events.dart';
+import 'package:myapp/page-1/new-events.dart';
+import 'package:myapp/page-1/frame-2608829.dart';
+import 'package:myapp/page-1/events-wG2.dart';
+import 'package:myapp/page-1/forum.dart';
+import 'package:myapp/page-1/event.dart';
+import 'package:myapp/page-1/map.dart';
+import 'package:myapp/page-1/newevent.dart';
+import 'package:myapp/page-1/profile.dart';
+import 'package:myapp/page-1/profile-Jog.dart';
 import 'package:myapp/page-1/registration.dart';
-// import 'package:myapp/page-1/login.dart';
-// import 'package:myapp/page-1/board.dart';
-// import 'package:myapp/page-1/calendar.dart';
-// import 'package:myapp/page-1/point.dart';
+import 'package:myapp/page-1/login.dart';
+import 'package:myapp/page-1/board.dart';
+import 'package:myapp/page-1/calendar.dart';
+import 'package:myapp/page-1/point.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
